b2aa4f11a4f1eff810041310da75673b6f0a3dd5
Seth Ek <sge3141@gmail.com>
2025-07-09 20:28:48 -0500

    Template for login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,14 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    // const { username, password } = req.body;
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log("Username:", username);
-    console.log("Password:", password);
-    res.status(200).json({ message: "Credentials received!" });
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
-  }
+    if (req.method === 'POST') {
+        const username = req.body.username;
+        const password = req.body.password;
+        console.log("Username:", username);
+        console.log("Password:", password);
+
+        // TODO: Talk to Supabase
+        if (1) {
+            res.status(200).json({ message: "Login successful" });
+        }
+    } 
+    
+    // Login failed
+    else {
+        res.status(401).json({ message: "Login failed. Please try again" });
+    }
 }
