611005ca20745bd591e4fc8b4acef69e97ae712e
25amaroa <anafcw@umsystem.edu>
2025-09-24 13:20:58 -0500

    Homepage update

--- app/components/LinkAsButton.tsx
+++ app/components/LinkAsButton.tsx
@@ -4,19 +4,38 @@ import React from 'react'
 interface LinkAsButtonProps {
   onClick?: () => void
   children?: React.ReactNode
-  className?: string,
-  href?: string,
+  className?: string
+  href?: string
+  size?: "sm" | "md" | "lg" // Select sizing for buttons
 }
 
-export const LinkAsButton: React.FC<LinkAsButtonProps> = ({ onClick, children, className = '', href= ''}) => {
-  
-    const baseClass = "px-4 py-2 bg-amber-400 text-white rounded hover:bg-amber-800 transition: duration-200"
-    
-    return (
-      <Link href={`${href}`}>
-        <button onClick={onClick} className={`${baseClass} ${className}`}>
-          {children}
-        </button>
-      </Link>
+export const LinkAsButton: React.FC<LinkAsButtonProps> = ({ // sorry the other long format hurt my brain
+  onClick,
+  children,
+  className = '',
+  href = '',
+  size = "md"
+}) => {
+  let sizeClass = "";
+  switch (size) {
+    case "sm":
+      sizeClass = "w-16 h-16 text-base";
+      break;
+    case "md":
+      sizeClass = "w-35 h-35 text-lg";
+      break;
+    case "lg":
+      sizeClass = "w-50 h-50 text-xl";
+      break;
+  }
+
+  const baseClass = `flex items-center justify-center bg-amber-400 text-white rounded-full hover:bg-amber-800 transition duration-200 ${sizeClass}`
+
+  return (
+    <Link href={href}>
+      <button onClick={onClick} className={`${baseClass} ${className}`}>
+        {children}
+      </button>
+    </Link>
   )
 }
