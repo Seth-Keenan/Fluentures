baca4c79a093e4ef2adb10ed4769ab382ce885c1
Seth Ek <sge3141@gmail.com>
2025-10-05 14:36:49 -0500

    Made a lot of changes with the supabase clients and edited app/lib/actions/wordlistAction.ts

---
+++ app/lib/hooks/supabaseServerActionClient.ts
@@ -0,0 +1,8 @@
+// app/lib/hooks/supabaseServerActionClient.ts
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export async function getSupabaseServerActionClient() {
+  const cookieStore = await cookies();  
+  return createServerActionClient({ cookies: () => cookieStore }); // this is actually correct, not an error
+}
